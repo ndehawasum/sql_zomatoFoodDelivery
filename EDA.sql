c1a0cbd6c3cda778cbf3e727631f5c33eaa78622
Ndeh Awasum <128726014+ndehawasum@users.noreply.github.com>
2026-02-06 22:05:39 -0600

    Update EDA.sql
    Checked for Null values. The delivery table is the only table with null values. These null values are delivery times for orders that were not delivered. These null values will be removed in case they pose a problem in the future.

--- EDA.sql
+++ EDA.sql
@@ -7,5 +7,43 @@ SELECT * FROM orders;
 SELECT * FROM riders;
 SELECT * FROM deliveries;
 
--- Import datasets
+-- Handling Null values
 
+SELECT COUNT(*) FROM customers
+WHERE 
+    customer_name IS NULL
+    OR
+    reg_date IS NULL
+
+SELECT COUNT(*) FROM restaurants
+WHERE 
+    restaurant_name IS NULL
+    OR
+    city IS NULL
+    OR
+    opening_hours IS NULL
+
+SELECT COUNT(*) FROM orders
+WHERE 
+    order_item IS NULL
+    OR
+    order_date IS NULL
+    OR
+    order_time IS NULL
+    OR
+    order_status IS NULL
+    OR 
+    total_amount IS NULL
+
+SELECT COUNT(*) FROM riders
+WHERE 
+    rider_name IS NULL
+    OR
+    sign_up IS NULL
+
+
+SELECT COUNT(*) FROM deliveries
+WHERE 
+    delivery_status IS NULL
+    OR
+    delivery_time IS NULL
